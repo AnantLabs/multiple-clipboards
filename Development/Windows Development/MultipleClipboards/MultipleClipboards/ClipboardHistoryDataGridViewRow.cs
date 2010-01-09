74166470ff13ce9bd8838b04cc8a068a42c260eb
eric <eric@ba7c2276-fe06-11de-9d47-03d50c098576>
2010-01-09 19:55:29 +0000

    Forgot to add these 2 new files.

---
+++ Development/Windows Development/MultipleClipboards/MultipleClipboards/ClipboardHistoryDataGridViewRow.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MultipleClipboards
+{
+	/// <summary>
+	/// A Data Grid View Row object specifically for clipboard history entries.
+	/// </summary>
+	public class ClipboardHistoryDataGridViewRow : DataGridViewRow
+	{
+		private int _clipboardHistoryIndex;
+
+		/// <summary>
+		/// Constructs a new ClipbaordHistoryDataGridView object.
+		/// </summary>
+		/// <param name="clipboardHistoryIndex">The index of the clipboard history item in the history queue.</param>
+		/// <param name="gridView">A reference to the grid that this row will be a part of.</param>
+		/// <param name="visibleIndex">The index of the clipboard history item that will display in the grid.</param>
+		/// <param name="text">The text to display in the grid.</param>
+		/// <param name="timestamp">The timestamp of the item.</param>
+		public ClipboardHistoryDataGridViewRow(int clipboardHistoryIndex, DataGridView gridView, int visibleIndex, string text, DateTime timestamp)
+			: base()
+		{
+			this._clipboardHistoryIndex = clipboardHistoryIndex;
+			this.CreateCells(gridView, new object[] { visibleIndex.ToString(), text, timestamp.ToShortTimeString() });
+		}
+
+		/// <summary>
+		/// Gets the index for this item in the clipboard history queue.
+		/// </summary>
+		public int ClipboardHistoryIndex
+		{
+			get
+			{
+				return this._clipboardHistoryIndex;
+			}
+		}
+	}
+}
---
+++ Development/Windows Development/MultipleClipboards/MultipleClipboards/ClipboardHistoryToolStripMenuItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MultipleClipboards
+{
+	/// <summary>
+	/// A Tool Strip Menu Item object specifically for clipboard history entries.
+	/// </summary>
+	public class ClipboardHistoryToolStripMenuItem : ToolStripMenuItem
+	{
+		private int _clipboardHistoryIndex;
+
+		/// <summary>
+		/// Constructs a new ClipboardHistoryToolStripMenuItem.
+		/// </summary>
+		/// <param name="text">The text to display on the menu item.</param>
+		/// <param name="clipboardHistoryIndex">The index for this item in the clipboard history queue.</param>
+		/// <param name="onClick">The on click event handler for this item.</param>
+		public ClipboardHistoryToolStripMenuItem(string text, int clipboardHistoryIndex, EventHandler onClick)
+			: base(text, null, onClick)
+		{
+			this._clipboardHistoryIndex = clipboardHistoryIndex;
+		}
+
+		/// <summary>
+		/// Gets the index for this item in the clipboard history queue.
+		/// </summary>
+		public int ClipboardHistoryIndex
+		{
+			get
+			{
+				return this._clipboardHistoryIndex;
+			}
+		}
+	}
+}
