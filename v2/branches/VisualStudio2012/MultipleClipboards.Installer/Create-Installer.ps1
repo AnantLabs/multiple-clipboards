9bf27082d645674df7029b84e6625a8d60d62832
eric.coonradt@gmail.com <eric.coonradt@gmail.com@ba7c2276-fe06-11de-9d47-03d50c098576>
2012-11-10 16:43:56 +0000

    Finally commit all the work I've done on this.  This should have been pushed incrementally, but I can't change it now.  Anyway, the solution works and builds in Visual Studio Express 2012 and I have a crude WIX installer working.  The installer still needs polishing and testing before this is ready to merge into trunk.

---
+++ v2/branches/VisualStudio2012/MultipleClipboards.Installer/Create-Installer.ps1
@@ -0,0 +1,104 @@
+﻿Param
+(
+    [boolean]$useReleaseMode = $false,
+    [string]$installerRelativePath = ".\MultipleClipboards.Installer",
+    [string]$inputWxsFile = "Installer.wxs",
+    [string]$objectFileName = "MultipleClipboards.Installer.wixobj",
+    [string]$msiFileName = "MultipleClipboards.Installer.msi"
+)
+
+function WriteErrorAndExit ($message, $popLocation = $false)
+{
+    if ($popLocation)
+    {
+        Pop-Location
+    }
+
+    Write-Host $message -ForegroundColor Red
+    Write-Host ""
+    exit
+}
+
+Write-Host ""
+
+if (!(Test-Path "MultipleClipboards.sln"))
+{
+    WriteErrorAndExit "This script must be run from the root of a MultipleClipboards branch."
+}
+
+Write-Host "Cleaning old installer" -ForegroundColor Cyan
+$outputObjPath = $installerRelativePath + "\obj"
+$outputBinPath = $installerRelativePath + "\bin"
+
+if (Test-Path $outputObjPath)
+{
+    Get-ChildItem $outputObjPath -Recurse | foreach ($_) { Remove-Item $_.FullName }
+}
+else
+{
+    New-Item $outputObjPath -ItemType Directory | Out-Null
+}
+
+if (Test-Path $outputBinPath)
+{
+    Get-ChildItem $outputBinPath -Recurse | foreach ($_) { Remove-Item $_.FullName }
+}
+else
+{
+    New-Item $outputBinPath -ItemType Directory | Out-Null
+}
+
+Push-Location
+$mode = ""
+
+if ($useReleaseMode)
+{
+    if (!(Test-Path .\MultipleClipboards\bin\Release))
+    {
+        WriteErrorAndExit "Release mode path does not exist.  Ensure that Multiple Clipboards has been built in release mode and try again."
+    }
+
+    $mode = "release"
+    cd .\MultipleClipboards\bin\Release
+}
+else
+{
+    if (!(Test-Path .\MultipleClipboards\bin\Debug))
+    {
+        WriteErrorAndExit "Debug mode path does not exist.  Ensure that Multiple Clipboards has been built in debug mode and try again."
+    }
+
+    $mode = "debug"
+    cd .\MultipleClipboards\bin\Debug
+}
+
+$inputFilePath = "..\..\." + $installerRelativePath + "\" + $inputWxsFile
+$outputObjPath = "..\..\." + $outputObjPath + "\" + $objectFileName
+$outputBinPath = "..\..\." + $outputBinPath + "\" + $msiFileName
+
+Write-Host "Using the $mode mode build of Multiple Clipboards" -ForegroundColor Cyan
+Write-Host "Compiling Installer..." -ForegroundColor Cyan
+
+if (Get-Command "candle.exe")
+{
+    candle.exe -o $outputObjPath $inputFilePath
+}
+else
+{
+    WriteErrorAndExit "Unable to find 'candle.exe'.  Ensure WIX is installed and its bin directory has been added to the system path." $true
+}
+
+Write-Host "Linking Installer..." -ForegroundColor Cyan
+
+if (Get-Command "light.exe")
+{
+    light.exe -ext WixUIExtension -ext WixNetFxExtension -o $outputBinPath $outputObjPath
+}
+else
+{
+    WriteErrorAndExit "Unable to find 'light.exe'.  Ensure WIX is installed and its bin directory has been added to the system path." $true
+}
+
+Pop-Location
+Write-Host "Done." -ForegroundColor Cyan
+Write-Host ""
---
+++ v2/branches/VisualStudio2012/MultipleClipboards.WixCustomActions/CustomActions.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Forms;
+using Microsoft.Deployment.WindowsInstaller;
+using MultipleClipboards.GlobalResources;
+
+namespace MultipleClipboards.WixCustomActions
+{
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult CheckForRunningInstances(Session session)
+        {
+            if (!EnvironmentHelper.IsMultipleClipboardsRunning())
+            {
+                session["APP_RUNNING"] = "0";
+                return ActionResult.Success;
+            }
+
+            const string message = "There is already an instance of Multiple Clipboards running.\r\n\r\nWould you like us to terminate the running instance and proceed with the install?";
+
+            if (MessageBox.Show(message, "Multiple Clipboards Already Running", MessageBoxButtons.YesNo) != DialogResult.Yes)
+            {
+                session["APP_RUNNING"] = "1";
+                return ActionResult.UserExit;
+            }
+
+            try
+            {
+                EnvironmentHelper.KillRunningMultipleClipboardsInstances();
+                session["APP_RUNNING"] = "0";
+                return ActionResult.Success;
+            }
+            catch
+            {
+                session["APP_RUNNING"] = "1";
+                return ActionResult.Failure;
+            }
+        }
+    }
+}
